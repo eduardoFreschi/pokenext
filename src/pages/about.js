de42f8c54b50390f1c65a46f52c44f80a555af6a
eduardoFreschi <eduardo.f.freschi@gmail.com>
2023-05-03 16:47:22 -0300

    update project

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import styles from "@/styles/About.module.css";
+
 export default function About() {
   return (
     <div className={styles.about}>
@@ -9,7 +10,7 @@ export default function About() {
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.{" "}
+        scrambled it to make a type specimen book.
       </p>
       <Image
         src="/images/charizard.png"
